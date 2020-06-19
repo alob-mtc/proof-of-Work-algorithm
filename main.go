c34ecd2f7f4e12bd9969ca05e95bef9577158a7a
Akinlua Bolamigbe Jomiloju <bolamigbeakinlua@gmail.com>
2020-06-19 16:38:22 +0100

    checking bit-wise

--- main.go
+++ main.go
@@ -10,25 +10,39 @@ var characterSet = []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1
 
 func main() {
 	start := time.Now()
-	seed := uint64(3)
 	prefix := []byte("Bola is a boy")
-	var str []byte
-	hashes := 0
-	random := make([]byte, 20)
-	random = append(prefix, random...)
-	for {
-		hashes++
-		seed, str = RandomString(random, len(prefix), seed)
-		if Hash(random, 3) {
-			fmt.Println(string(str))
-			break
-		}
+	numberOfGoR := 4
+	hashes := make([]int, numberOfGoR)
+	solutionChan := make(chan []byte, 1)
+
+	for i := 0; i < numberOfGoR; i++ {
+		go func(index int) {
+			offset := len(prefix)
+			random := make([]byte, 20)
+			random = append(prefix, random...)
+			seed := uint64(index + 1)
+			for {
+				hashes[index]++
+				seed = RandomString(random, offset, seed)
+				if Hash(random, 27) {
+					solutionChan <- random
+					break
+				}
+			}
+		}(i)
+	}
+
+	solution := <-solutionChan
+	fmt.Println(string(solution))
+	var totalHashesProcessed int
+	for _, val := range hashes {
+		totalHashesProcessed += val
 	}
 	end := time.Now()
 	time := end.Sub(start).Seconds()
 	fmt.Println("total time:", time)
-	fmt.Println("No of hashes:", hashes)
-	fmt.Printf("processed/sec: %d\n", int64(float64(hashes)/time))
+	fmt.Println("No of hashes:", totalHashesProcessed)
+	fmt.Printf("processed/sec: %d\n", int64(float64(totalHashesProcessed)/time))
 }
 
 // RandomNumber f
@@ -40,21 +54,25 @@ func RandomNumber(seed uint64) uint64 {
 }
 
 // RandomString x
-func RandomString(str []byte, offset int, seed uint64) (uint64, []byte) {
+func RandomString(str []byte, offset int, seed uint64) uint64 {
 	for i := offset; i < len(str); i++ {
 		seed = RandomNumber(seed)
 		str[i] = characterSet[seed%62]
 	}
-	return seed, str
+	return seed
 }
 
 // Hash func
-func Hash(input []byte, challengeLength int) bool {
+// ? i don't know how this fully works => checking bit-wise
+func Hash(input []byte, bits int) bool {
 	hash := sha256.Sum256(input)
-	for i := 0; i < challengeLength; i++ {
-		if hash[i] > 0 {
+	nbytes := bits / 8
+	nbits := bits % 8
+	var idx int //this init it to 0
+	for ; idx < nbytes; idx++ {
+		if hash[idx] > 0 {
 			return false
 		}
 	}
-	return true
+	return (hash[idx] >> (8 - nbits)) == 0
 }
