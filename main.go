c9dc295f79646eb1f602ed88a253a9c07c111c77
Akinlua Bolamigbe Jomiloju <bolamigbeakinlua@gmail.com>
2020-06-22 01:59:48 +0100

    added the generator routines that handles generating random string added the routines for hashing -> they pick up random string generated by the gen and process(hash) them preallocation all the slice and using them in the routines -> improve the speed

--- main.go
+++ main.go
@@ -4,34 +4,23 @@ import (
 	"crypto/sha256"
 	"fmt"
 	"time"
+
+	humanize "github.com/dustin/go-humanize"
 )
 
 var characterSet = []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
+var hashes = []int{}
 
 func main() {
 	start := time.Now()
-	prefix := []byte("Bola is a boy")
-	numberOfGoR := 4
-	hashes := make([]int, numberOfGoR)
-	solutionChan := make(chan []byte, 1)
-
-	for i := 0; i < numberOfGoR; i++ {
-		go func(index int) {
-			offset := len(prefix)
-			random := make([]byte, 20)
-			random = append(prefix, random...)
-			seed := uint64(index + 1)
-			for {
-				hashes[index]++
-				seed = RandomString(random, offset, seed)
-				if Hash(random, 27) {
-					solutionChan <- random
-					break
-				}
-			}
-		}(i)
-	}
-
+	prefix := []byte("Bola Is A Boy")
+	numberOfGoRHasher := 4
+	numberOfGoRGen := 1
+	solutionChan := make(chan []byte)
+	// spin up the process
+	// for i := 0; i < 3; i++ {
+		POWOnCore(prefix, numberOfGoRGen, numberOfGoRHasher, solutionChan)
+	// }
 	solution := <-solutionChan
 	fmt.Println(string(solution))
 	var totalHashesProcessed int
@@ -41,8 +30,60 @@ func main() {
 	end := time.Now()
 	time := end.Sub(start).Seconds()
 	fmt.Println("total time:", time)
-	fmt.Println("No of hashes:", totalHashesProcessed)
-	fmt.Printf("processed/sec: %d\n", int64(float64(totalHashesProcessed)/time))
+	fmt.Println("No of hashes:", humanize.Comma(int64(totalHashesProcessed)))
+	fmt.Printf("processed/sec: %s\n", humanize.Comma(int64(float64(totalHashesProcessed)/time)))
+}
+
+// POWOnCore x
+func POWOnCore(prefix []byte, numberOfGan, numberOfHasher int, solutionChan chan []byte) {
+	blockSize := 1024
+	size := numberOfHasher * 2
+	// the precess channels
+	unprocessIndex := make(chan int, size)
+	processIndex := make(chan int, size)
+
+	// blocks
+	blocks := make([][][]byte, size)
+	offset := len(prefix)
+	for idx := range blocks {
+		unprocessIndex <- idx
+		blocks[idx] = make([][]byte, blockSize)
+		for i := 0; i < blockSize; i++ {
+			blocks[idx][i] = make([]byte, 20)
+			blocks[idx][i] = append(prefix, blocks[idx][i]...)
+		}
+	}
+
+	// Random string generator
+	for i := 0; i < numberOfGan; i++ {
+		go func() {
+			seed := uint64(time.Now().Local().UnixNano())
+			for blockIndex := range unprocessIndex {
+				for _, val := range blocks[blockIndex] {
+					seed = RandomString(val, offset, seed)
+				}
+				processIndex <- blockIndex
+			}
+		}()
+	}
+	// spin up the hashers
+	for i := 0; i < numberOfHasher; i++ {
+		index := len(hashes)
+		hashes = append(hashes, 0)
+		go func(hashIndex int) {
+			for blockIndex := range processIndex {
+				hashes[hashIndex] += blockSize
+				for _, val := range blocks[blockIndex] {
+					if Hash(val, 27) {
+						solutionChan <- val
+						break
+					}
+				}
+				// return the index to the unprocessIndex chan => a new set of [prefix+randomString]byte is generated
+				unprocessIndex <- blockIndex
+			}
+		}(index)
+	}
 }
 
 // RandomNumber f
